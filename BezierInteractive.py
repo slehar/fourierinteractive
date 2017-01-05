ecc133e44ead3315eff4507d080b0a1b070a0786
Steven Lehar <slehar@gmail.com>
2017-01-05 10:08:39 -0500

    fix sum/prod and thresh

--- BezierInteractive.py
+++ BezierInteractive.py
@@ -3,9 +3,10 @@
 BezierInteractive.py
 
 Not actually a true Bezier curve, this is an interactive exploration
-of Radial and Circumferential periodicity, allowing the radial/circm
-frequency to be adjusted by sliders, and the radial and circm images
-are combined additively and multiplicatively.
+of Radial and Angular periodicity, allowing the radial/angular
+frequency to be adjusted by sliders, and the radial and angular images
+are combined additively and multiplicatively, also controlled by 
+sliders.
 
 Created on Mon Dec 19 09:37:14 2016
 
@@ -16,11 +17,12 @@ import matplotlib.pyplot as plt
 from   matplotlib.widgets import Slider
 from   matplotlib.widgets import CheckButtons
 import numpy as np
+import numpy.ma as ma
 import sys
 
 # Global Variables
 freqRad = 25.
-freqCirc = 10.
+freqAng = 10.
 slidersLocked = False
 sumVal  = .5
 prodVal = .5
@@ -55,9 +57,6 @@ def func(label):
     
 check.on_clicked(func)
 
-# Image dimensions
-ySize, xSize = (512,512)
-hafY, hafX = int(ySize/2), int(xSize/2)
 
 # Axes for  Radial
 axRad = fig.add_axes([.1, .2, .5/winAspect, .7])
@@ -65,11 +64,11 @@ axRad.axes.set_xticks([])
 axRad.axes.set_yticks([])
 axRad.set_title('Radial')
 
-# Axes for  Circum
-axCir = fig.add_axes([.4, .2, .5/winAspect, .7])
-axCir.axes.set_xticks([])
-axCir.axes.set_yticks([])
-axCir.set_title('Circumf')
+# Axes for  Angular
+axAng = fig.add_axes([.4, .2, .5/winAspect, .7])
+axAng.axes.set_xticks([])
+axAng.axes.set_yticks([])
+axAng.set_title('Circumf')
 
 # Axes for Mask
 axMask = fig.add_axes([.7, .2, .5/winAspect, .7])
@@ -79,6 +78,8 @@ axMask.set_title('Mask')
 
 
 #### Bezier ####
+ySize, xSize = (512,512)
+hafY, hafX = int(ySize/2), int(xSize/2)
 yy, xx = np.mgrid[-hafY:hafY, -hafX:hafX]
 
 distImg = np.sqrt(xx**2 + yy**2)
@@ -87,8 +88,8 @@ plt.sca(axRad)
 radialPlot = plt.imshow(radialImg, cmap='gray')
 
 angleImg = np.arctan2(yy,xx)
-circumImg = np.cos(angleImg * float(int(freqCirc)))
-plt.sca(axCir)
+circumImg = np.cos(angleImg * float(int(freqAng)))
+plt.sca(axAng)
 circPlot = plt.imshow(circumImg, cmap='gray')
 
 multImg = (radialImg * circumImg)
@@ -107,8 +108,8 @@ axSlider2.set_xticks([])
 axSlider2.set_yticks([])
 
 slider1 = Slider(axSlider1, 'radial', 0.0, 50., valinit=25.)
-slider2 = Slider(axSlider2, 'circum', 0.0, 20., valinit=10.)
-freqRad, freqCirc = slider1.val, slider2.val
+slider2 = Slider(axSlider2, 'angular', 0.0, 20., valinit=10.)
+freqRad, freqAng = slider1.val, slider2.val
 
 # Filter angular sliders
 axSlider3 = fig.add_axes([0.7, 0.125, 0.234, 0.04])
@@ -120,9 +121,9 @@ axSlider4 = fig.add_axes([0.7, 0.05, 0.237, 0.04])
 axSlider4.set_xticks([])
 axSlider4.set_yticks([])
 
-slider3 = Slider(axSlider3, 'sum',  0., 1., valinit=.5)
-slider4 = Slider(axSlider4, 'product', 0., 1., valinit=.5)
-sumVal, prodVal = slider3.val, slider4.val
+slider3 = Slider(axSlider3, 'sum/prod',  0., 1., valinit=.5)
+slider4 = Slider(axSlider4, 'thresh',    0., 1., valinit=0.)
+sumProd, thresh = slider3.val, slider4.val
 
 def update():
     
@@ -130,16 +131,20 @@ def update():
     plt.sca(axRad)
     radialPlot.set_data(radialImg)
 
-    circImg = np.cos(angleImg * float(int(freqCirc)))
-    plt.sca(axCir)
+    circImg = np.cos(angleImg * float(int(freqAng)))
+    plt.sca(axAng)
     circPlot.set_data(circImg)
 
     sumImg  = (radialImg + circImg)
     prodImg = (radialImg * circImg)
     mergeImg = (sumVal * sumImg) + (prodVal * prodImg)
 
+    maskImg = (np.fabs(mergeImg) > thresh)
+    xmask = ma.make_mask(maskImg)
+    filtImg = mergeImg * xmask
+
     plt.sca(axMask)
-    mergePlot.set_data(mergeImg)
+    mergePlot.set_data(filtImg)
     plt.pause(.001)
 
 def update1(val):
@@ -148,22 +153,23 @@ def update1(val):
     update()
 
 def update2(val):
-    global freqCirc
-    diff = max((freqRad - freqCirc), 0.)
-    freqCirc = slider2.val
+    global freqAng
+    diff = max((freqRad - freqAng), 0.)
+    freqAng = slider2.val
     if slidersLocked:
-        val1 = freqCirc - diff
+        val1 = freqAng - diff
         slider1.set_val(val1)
     update()
 
 def update3(val):
-    global sumVal
+    global sumVal, prodVal
     sumVal = slider3.val
+    prodVal = 1. - sumVal
     update()
 
 def update4(val):
-    global prodVal
-    prodVal = slider4.val
+    global thresh
+    thresh = slider4.val
     update()
 
 #    fig.canvas.draw()
@@ -173,8 +179,6 @@ slider3.on_changed(update3)
 slider4.on_changed(update4)
 
 # Show image
-plt.ion()
-#plt.pause(.001)
 plt.show()
 
 # Pop fig window to top]]
